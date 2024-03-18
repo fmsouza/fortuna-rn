b42511657fe62a0454370eecd29fd8772b9af36c
Fred Souza <fredericoamsouza@gmail.com>
2024-03-18 11:54:23 +0000

    Rebuilding DailyBarView

--- modules/transactions/components/charts/DailyBarView.tsx
+++ modules/transactions/components/charts/DailyBarView.tsx
@@ -1,7 +1,6 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { useTheme } from "react-native-paper";
-import { CartesianChart, Bar } from "victory-native";
-import { useFont } from "@shopify/react-native-skia";
+import { VictoryChart, VictoryTheme, VictoryBar, VictoryAxis } from "victory-native";
 
 import { Theme } from "~/theme";
 import { Transaction } from "~/modules/transactions/types";
@@ -12,39 +11,36 @@ type DailyBarViewBarViewProps = {
 };
 export function DailyBarViewBarView({ transactions }: DailyBarViewBarViewProps) {
   const theme = useTheme() as Theme;
-  const font = useFont(require("./roboto-regular.ttf"));
+
   const labels = useMemo(() => listDaysInMonth(transactions[0].registeredAt), [transactions]);
   const aggregatedTransactionValues = useMemo(() => aggregateByDayOfMonth(transactions), [transactions]);
   const records = useMemo(() => labels.map((dayOfMonth) => ({
     dayOfMonth,
-    amount: aggregatedTransactionValues[dayOfMonth],
+    amount: aggregatedTransactionValues[dayOfMonth] ?? 0,
   })), [aggregatedTransactionValues, labels]);
 
   return (
-    <CartesianChart 
-      data={records}
-      xKey="dayOfMonth"
-      yKeys={["amount"]}
-      padding={{ top: 0, right: 0, bottom: 10, left: 0 }}
-      axisOptions={{
-        font,
-        tickCount: { x: 15, y: 0 },
-        formatXLabel: (value) => {
-          const date = new Date();
-          date.setDate(value + 1);
-          return date.toLocaleString("default", { day: "numeric" });
-        },
-      }}
+    <VictoryChart
+      theme={VictoryTheme.material}
+      width={theme.viewport.width}
+      height={theme.viewport.height / 4}
     >
-      {({ points, chartBounds }) => (
-        <Bar
-          antiAlias
-          points={points.amount}
-          chartBounds={chartBounds}
-          color={theme.colors.primary}
-          roundedCorners={{ topLeft: 50, topRight: 50 }}
-        />
-      )}
-    </CartesianChart>
+      <VictoryBar
+        style={{ data: { fill: theme.colors.primary } }}
+        cornerRadius={{ top: 4 }}
+        barWidth={8}
+        data={records}
+        x="dayOfMonth"
+        y="amount"
+      />
+      <VictoryAxis
+        style={{
+          ticks: { display: "none" },
+          grid: { stroke: theme.colors.primary, strokeOpacity: 0.4 },
+          axisLabel: { display: "none" },
+        }}
+        tickFormat={(t) => `${new Date(0, 0, t + 1).toLocaleString("default", { day: "numeric" })}`}
+      />
+    </VictoryChart>
   );
 };
