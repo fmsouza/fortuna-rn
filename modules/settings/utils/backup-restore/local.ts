36d7ef13c5a411b123615de2b1bf923d781ecc28
Fred Souza <fredericoamsouza@gmail.com>
2024-03-22 19:57:46 +0000

    Fixing Android local backup

--- modules/settings/utils/backup-restore/local.ts
+++ modules/settings/utils/backup-restore/local.ts
@@ -3,6 +3,7 @@ import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 import * as Updates from 'expo-updates';
+import { Platform } from 'react-native';
 
 import { DB_NAME } from '~/db';
 
@@ -12,32 +13,56 @@ export async function backupToLocal() {
   const timestamp = dayjs().format('DD_MM_YYYY_HHmmss');
   const [dbName, extension] = DB_NAME.split('.');
   const fileName = `${dbName}_backup_${timestamp}.${extension}`;
-  const destinationPath = FileSystem.documentDirectory + fileName;
+  const backupCopyPath = FileSystem.documentDirectory + fileName;
 
-  try {
-    await FileSystem.copyAsync({
-      from: CURRENT_DB_PATH,
-      to: destinationPath
-    });
-    const dbFile = await FileSystem.getInfoAsync(destinationPath);
+  await FileSystem.copyAsync({
+    from: CURRENT_DB_PATH,
+    to: backupCopyPath
+  });
+  const dbBackupFile = await FileSystem.getInfoAsync(backupCopyPath);
 
-    await Sharing.isAvailableAsync();
-    await Sharing.shareAsync(`file://${dbFile.uri}`);
-  } catch (e) {
-    console.log(e);
+  switch (Platform.OS) {
+
+    case 'android': {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (!permissions.granted) {
+          throw new Error('Permission denied');
+      }
+
+      const destinationUri = await FileSystem.StorageAccessFramework.createFileAsync(
+        permissions.directoryUri,
+        fileName,
+        'application/octet-stream'
+      );
+
+      const contentAsString = await FileSystem.readAsStringAsync(dbBackupFile.uri, { encoding: FileSystem.EncodingType.Base64 });
+
+      await FileSystem.StorageAccessFramework.writeAsStringAsync(
+        destinationUri,
+        contentAsString,
+        { encoding: FileSystem.EncodingType.Base64 }
+      );
+      break;
+    }
+
+    case 'ios': {
+      await Sharing.isAvailableAsync();
+      await Sharing.shareAsync(`file://${dbBackupFile.uri}`);
+      break;
+    }
   }
 }
 
 export async function restoreFromLocal() {
-  const dbFile = await DocumentPicker.getDocumentAsync({
+  const dbBackupFile = await DocumentPicker.getDocumentAsync({
     type: 'application/octet-stream',
   });
   
-  if (dbFile.canceled) {
+  if (dbBackupFile.canceled) {
     return;
   }
 
-  const [{uri}] = dbFile.assets;
+  const [{uri}] = dbBackupFile.assets;
 
   await FileSystem.copyAsync({
     from: uri,
